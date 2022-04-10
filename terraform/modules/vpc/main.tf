490d4a0055b15493a80da8b6b852062d3d4101da
Evgeniy Alesh <ayden1st@gmail.com>
2022-04-10 23:54:04 +0300

    Fix for tests

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.73.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     yandex = {
+#       source  = "yandex-cloud/yandex"
+#       version = "0.73.0"
+#     }
+#   }
+# }
 
 resource "yandex_vpc_network" "app-network" {
   name = "app-network"
