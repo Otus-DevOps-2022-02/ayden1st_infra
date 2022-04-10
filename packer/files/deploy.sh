82de8ae6646be19f395359c15b7bcb177e7ae123
Evgeniy Alesh <ayden1st@gmail.com>
2022-04-10 23:13:10 +0300

    Change packer config and scripts

--- packer/files/deploy.sh
+++ packer/files/deploy.sh
@@ -5,9 +5,9 @@ cd /opt
 git clone -b monolith https://github.com/express42/reddit.git
 addgroup --system puma
 adduser --system --home /opt/reddit --shell /bin/nologin --ingroup puma --no-create-home puma
+chown -R puma:puma reddit
 cd reddit
 bundle install
-chown -R puma:puma reddit
 cp /tmp/puma.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable --now puma.service
