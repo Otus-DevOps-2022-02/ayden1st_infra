e137f1aed2776ad7a763bf1fdad81d382f2b3a5c
Evgeniy Alesh <ayden1st@gmail.com>
2022-04-10 23:49:57 +0300

    Add module vpc to env

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -14,12 +14,17 @@ provider "yandex" {
   zone                     = var.zone
 }
 
+module "vpc" {
+  source           = "../modules/vpc"
+  zone = var.zone
+}
+
 module "app" {
   source           = "../modules/app"
   public_key_path  = var.public_key_path
   private_key_path = var.private_key_path
   app_disk_image   = var.app_disk_image
-  subnet_id        = var.subnet_id
+  subnet_id        = module.vpc.subnet_id
   db_ip            = module.db.internal_ip_address_db
 }
 
@@ -28,5 +33,5 @@ module "db" {
   public_key_path  = var.public_key_path
   private_key_path = var.private_key_path
   db_disk_image    = var.db_disk_image
-  subnet_id        = var.subnet_id
+  subnet_id        = module.vpc.subnet_id
 }
